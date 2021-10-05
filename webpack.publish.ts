db31802994884975a262a353132302385fbb36fa
CapitanFindusFI <am95.dev@gmail.com>
2021-10-05 10:31:38 +0200

    created specific webpack configuration for publishing

---
+++ webpack.publish.ts
@@ -0,0 +1,33 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+module.exports = {
+  entry: path.join(__dirname, "src", "lib", "index.ts"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "build.js",
+  },
+  devServer: {
+    port: 4000,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+  ],
+};
